30075cc56c3a15bbe2cb7f37f5f0922cfab30507
Charles Madere <charlesrmadere@gmail.com>
2021-04-28 20:20:33 +0900

    catch functionality implemented for FuntoonRepository

--- funtoonRepository.py
+++ funtoonRepository.py
@@ -1,3 +1,7 @@
+import json
+import os
+from typing import Dict
+
 import requests
 from requests import ConnectionError, HTTPError, Timeout
 from urllib3.exceptions import MaxRetryError, NewConnectionError
@@ -10,10 +14,34 @@ except:
 
 class FuntoonRepository():
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        funtoonApiUrl: str = 'https://funtoon.party/api',
+        funtoonRepositoryFile: str = 'CynanBotCommon/funtoonRepository.json'
+    ):
+        if not utils.isValidUrl(funtoonApiUrl):
+            raise ValueError(f'funtoonApiUrl argument is malformed: \"{funtoonApiUrl}\"')
+        elif not utils.isValidStr(funtoonRepositoryFile):
+            raise ValueError(f'funtoonRepositoryFile argument is malformed: \"{funtoonRepositoryFile}\"')
 
-    def pkmnBattle(self, userThatRedeemed: str, userToBattle: str, twitchChannel: str):
+        self.__funtoonApiUrl = funtoonApiUrl
+        self.__funtoonRepositoryFile = funtoonRepositoryFile
+
+    def getFuntoonToken(self, twitchChannel: str) -> str:
+        if not utils.isValidStr(twitchChannel):
+            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
+
+        twitchChannelJson = self.__readJson(twitchChannel)
+        if twitchChannelJson is None:
+            return None
+
+        token = twitchChannelJson['token']
+        if not utils.isValidStr(token):
+            raise ValueError(f'\"token\" value for \"{twitchChannel}\" in Funtoon repository file ({self.__funtoonRepositoryFile}) is malformed: \"{token}\"')
+
+        return token
+
+    def pkmnBattle(self, userThatRedeemed: str, userToBattle: str, twitchChannel: str) -> bool:
         if not utils.isValidStr(userThatRedeemed):
             raise ValueError(f'userThatRedeemed argument is malformed: \"{userThatRedeemed}\"')
         elif not utils.isValidStr(userToBattle):
@@ -21,7 +49,13 @@ class FuntoonRepository():
         elif not utils.isValidStr(twitchChannel):
             raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
 
+        funtoonToken = self.getFuntoonToken(twitchChannel)
+
+        if not utils.isValidStr(funtoonToken):
+            return False
+
         # TODO
+        return False
 
     def pkmnCatch(self, userThatRedeemed: str, twitchChannel: str):
         if not utils.isValidStr(userThatRedeemed):
@@ -29,4 +63,48 @@ class FuntoonRepository():
         elif not utils.isValidStr(twitchChannel):
             raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
 
-        # TODO
+        funtoonToken = self.getFuntoonToken(twitchChannel)
+
+        if not utils.isValidStr(funtoonToken):
+            return False
+
+        rawResponse = None
+        try:
+            rawResponse = requests.post(
+                url = f'{self.__funtoonApiUrl}/events/custom',
+                headers = {
+                    'Authorization': funtoonToken,
+                    'Content-Type': 'application/json'
+                },
+                json = {
+                    'channel': twitchChannel,
+                    'event': 'catch'
+                },
+                timeout = utils.getDefaultTimeout()
+            )
+        except (ConnectionError, HTTPError, MaxRetryError, NewConnectionError, Timeout) as e:
+            print(f'Exception occurred when attempting to post Funtoon catch event for \"{twitchChannel}\": {e}')
+            raise RuntimeError(f'Exception occurred when attempting to post Funtoon catch event for \"{twitchChannel}\": {e}')
+
+        return True
+
+    def __readJson(self, twitchChannel: str) -> Dict:
+        if not utils.isValidStr(twitchChannel):
+            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
+
+        if not os.path.exists(self.__funtoonRepositoryFile):
+            raise FileNotFoundError(f'Funtoon repository file not found: \"{self.__funtoonRepositoryFile}\"')
+
+        with open(self.__funtoonRepositoryFile, 'r') as file:
+            jsonContents = json.load(file)
+
+        if jsonContents is None:
+            raise IOError(f'Error reading from Funtoon repository file: \"{self.__funtoonRepositoryFile}\"')
+        elif len(jsonContents) == 0:
+            raise ValueError(f'JSON contents of Funtoon repository file \"{self.__funtoonRepositoryFile}\" is empty')
+
+        for key in jsonContents:
+            if key.lower() == twitchChannel.lower():
+                return jsonContents[key]
+
+        return None
