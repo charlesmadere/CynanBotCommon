a2bfd91028a177b62132d2abaa8d811a7caa9e30
Charles Madere <charlesrmadere@gmail.com>
2021-09-12 16:48:51 +0900

    soundEventsHelper progress

---
+++ soundEventsHelper.js
@@ -0,0 +1,10 @@
+var ws = new WebSocket("ws://192.168.1.2:8765/"),
+messages = document.createElement('ul');
+ws.onmessage = function (event) {
+var messages = document.getElementsByTagName('ul')[0],
+    message = document.createElement('li'),
+    content = document.createTextNode(event.data);
+message.appendChild(content);
+messages.appendChild(message);
+};
+document.body.appendChild(messages);
