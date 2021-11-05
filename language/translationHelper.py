d72185dd7157863a0d45fcd15bbdddf857223638
Charles Madere <charlesrmadere@gmail.com>
2021-11-05 22:59:57 +0900

    a few more language-related classes and files properly laid out and placed within the `language` subdirectory

--- language/translationHelper.py
+++ language/translationHelper.py
@@ -2,6 +2,7 @@ import json
 import random
 from json.decoder import JSONDecodeError
 from os import path
+from typing import Dict
 
 import requests
 from google.cloud import translate_v2 as translate
@@ -15,86 +16,14 @@ try:
     from CynanBotCommon.language.languagesRepository import LanguagesRepository
     from CynanBotCommon.language.translationApiSource import \
         TranslationApiSource
+    from CynanBotCommon.language.translationResponse import TranslationResponse
 except:
     import utils
 
     from language.languageEntry import LanguageEntry
     from language.languagesRepository import LanguagesRepository
     from language.translationApiSource import TranslationApiSource
-
-
-class TranslationResponse():
-
-    def __init__(
-        self,
-        originalLanguage: LanguageEntry,
-        translatedLanguage: LanguageEntry,
-        originalText: str,
-        translatedText: str,
-        translationApiSource: TranslationApiSource
-    ):
-        if originalLanguage is not None and not originalLanguage.hasIso6391Code():
-            raise ValueError(f'originalLanguage argument must be either None or have an ISO 639-1 code: \"{originalLanguage}\"')
-        elif translatedLanguage is not None and not translatedLanguage.hasIso6391Code():
-            raise ValueError(f'translatedLanguage argument must be either None or have an ISO 639-1 code: \"{translatedLanguage}\"')
-        elif not utils.isValidStr(originalText):
-            raise ValueError(f'originalText argument is malformed: \"{originalText}\"')
-        elif not utils.isValidStr(translatedText):
-            raise ValueError(f'translatedText argument is malformed: \"{translatedText}\"')
-        elif translationApiSource is None:
-            raise ValueError(f'translationApiSource argument is malformed: \"{translationApiSource}\"')
-
-        self.__originalLanguage: LanguageEntry = originalLanguage
-        self.__translatedLanguage: LanguageEntry = translatedLanguage
-        self.__originalText: str = originalText
-        self.__translatedText: str = translatedText
-        self.__translationApiSource: TranslationApiSource = translationApiSource
-
-    def getOriginalLanguage(self) -> LanguageEntry:
-        return self.__originalLanguage
-
-    def getOriginalText(self) -> str:
-        return self.__originalText
-
-    def getTranslatedLanguage(self) -> LanguageEntry:
-        return self.__translatedLanguage
-
-    def getTranslatedText(self) -> str:
-        return self.__translatedText
-
-    def getTranslationApiSource(self) -> TranslationApiSource:
-        return self.__translationApiSource
-
-    def hasOriginalLanguage(self) -> bool:
-        return self.__originalLanguage is not None
-
-    def hasTranslatedLanguage(self) -> bool:
-        return self.__translatedLanguage is not None
-
-    def toStr(self) -> str:
-        prefixText = ''
-
-        if self.hasOriginalLanguage():
-            if self.hasTranslatedLanguage():
-                firstLangText = ''
-                if self.__originalLanguage.hasFlag():
-                    firstLangText = self.__originalLanguage.getFlag()
-                else:
-                    firstLangText = self.__originalLanguage.getIso6391Code().upper()
-
-                secondLangText = ''
-                if self.__translatedLanguage.hasFlag():
-                    secondLangText = self.__translatedLanguage.getFlag()
-                else:
-                    secondLangText = self.__translatedLanguage.getIso6391Code().upper()
-
-                prefixText = f'[ {firstLangText} ➡ {secondLangText} ] '
-            elif self.__originalLanguage.hasFlag():
-                prefixText = f'[ {self.__originalLanguage.getFlag()} ]'
-            else:
-                prefixText = f'[ {self.__originalLanguage.getIso6391Code().upper()} ]'
-
-        return f'{prefixText}{self.__translatedText}'
+    from language.translationResponse import TranslationResponse
 
 
 class TranslationHelper():
@@ -103,7 +32,7 @@ class TranslationHelper():
         self,
         languagesRepository: LanguagesRepository,
         deepLAuthKey: str,
-        googleServiceAccountFile: str = 'CynanBotCommon/googleServiceAccount.json'
+        googleServiceAccountFile: str = 'CynanBotCommon/language/googleServiceAccount.json'
     ):
         if languagesRepository is None:
             raise ValueError(f'languagesRepository argument is malformed: \"{languagesRepository}\"')
@@ -132,14 +61,18 @@ class TranslationHelper():
             print(f'Exception occurred when attempting to fetch translation from DeepL for \"{text}\": {e}')
             raise RuntimeError(f'Exception occurred when attempting to fetch translation from DeepL for \"{text}\": {e}')
 
-        jsonResponse = None
+        jsonResponse: Dict = None
         try:
             jsonResponse = rawResponse.json()
         except JSONDecodeError as e:
             print(f'Exception occurred when attempting to decode DeepL\'s response for \"{text}\" into JSON: {e}')
             raise RuntimeError(f'Exception occurred when attempting to decode DeepL\'s response for \"{text}\" into JSON: {e}')
 
-        translationsJson = jsonResponse.get('translations')
+        if not utils.hasItems(jsonResponse):
+            print(f'DeepL\'s JSON response is null/empty for \"{text}\": {jsonResponse}')
+            raise ValueError(f'DeepL\'s JSON response is null/empty for \"{text}\": {jsonResponse}')
+
+        translationsJson: Dict = jsonResponse.get('translations')
         if not utils.hasItems(translationsJson):
             raise RuntimeError(f'DeepL\'s JSON response for \"{text}\" has missing or empty \"translations\" field: {jsonResponse}')
 
