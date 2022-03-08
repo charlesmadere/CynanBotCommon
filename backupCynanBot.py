53ddf8ef6126a4ecc04e302b43d11c6eb2718026
Charles Madere <charlesrmadere@gmail.com>
2022-03-07 19:15:45 -0800

    finally fixed remaining backup issues

--- backupCynanBot.py
+++ backupCynanBot.py
@@ -2,38 +2,45 @@ import os
 import shutil
 import sys
 from shutil import SameFileError
+from typing import List
 
 
-def find_files(src):
-    relevant_files = list()
+def findFiles(src) -> List:
+    relevantFiles = list()
 
     for root, dirs, files in os.walk(src):
-        relevant_files.extend(os.path.join(root, f) for f in files
+        relevantFiles.extend(os.path.join(root, f) for f in files
             if f.endswith(".json") or f.endswith(".sqlite"))
 
-    return relevant_files
+    return relevantFiles
 
-def copy_files(dest, file_list):
-    for file in file_list:
-        dir = os.path.join(dest, os.path.dirname(file))
-        os.makedirs(dir, exist_ok = True)
+def copyFiles(targetDirectory: str, fileList: List):
+    for file in fileList:
+        commonPath = os.path.commonpath([ targetDirectory, file ])
+        targetDirectoryFolderName = targetDirectory[len(commonPath):]
+        copyDestination = f'{commonPath}{os.path.sep}{targetDirectoryFolderName}'
+        os.makedirs(copyDestination, exist_ok = True)
 
         try:
-            shutil.copy2(file, dir)
+            shutil.copy2(file, copyDestination)
         except SameFileError as e:
-            print(f'Encountered crazy copy error with file \"{file}\" in dir \"{dir}\": {e}')
+            print(f'Encountered crazy copy error with file \"{file}\" in dir \"{copyDestination}\": {e}')
 
 def main():
-    args = sys.argv[1:]
+    args: List[str] = sys.argv[1:]
 
-    if not args or len(args) < 2:
+    if not args or len(args) != 2:
         print("./backupCynanBot.py <src> <dest>")
         sys.exit(1)
 
-    print(f'Copying from {args[0]} to {args[1]}')
+    sourceDirectory = os.path.normcase(os.path.normpath(args[0]))
+    targetDirectory = os.path.normcase(os.path.normpath(args[1]))
+    print(f'Copying from {sourceDirectory} to {targetDirectory}...')
 
-    file_list = find_files(args[0])
-    copy_files(args[1], file_list)
+    fileList = findFiles(sourceDirectory)
+    copyFiles(targetDirectory, fileList)
+
+    print(f'Finished copying {len(fileList)} file(s) from {sourceDirectory} to {targetDirectory}')
 
 if __name__ == '__main__':
     main()
