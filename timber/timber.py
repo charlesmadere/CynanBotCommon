0dcb3eebf559875e25a53f3aac4efc461e090d47
Charles Madere <charlesrmadere@gmail.com>
2022-04-19 19:42:01 -0700

    Timber is now event queue based

--- timber/timber.py
+++ timber/timber.py
@@ -1,4 +1,7 @@
+import asyncio
 import os
+from asyncio import AbstractEventLoop
+from queue import SimpleQueue
 
 try:
     import CynanBotCommon.utils as utils
@@ -13,16 +16,28 @@ class Timber():
 
     def __init__(
         self,
+        eventLoop: AbstractEventLoop,
         alsoPrintToStandardOut: bool = True,
+        sleepTimeSeconds: float = 0.25,
         timberRootDirectory: str = 'CynanBotCommon/timber'
     ):
-        if not utils.isValidBool(alsoPrintToStandardOut):
+        if eventLoop is None:
+            raise ValueError(f'eventLoop argument is malformed: \"{eventLoop}\"')
+        elif not utils.isValidBool(alsoPrintToStandardOut):
             raise ValueError(f'alsoPrintToStandardOut argument is malformed: \"{alsoPrintToStandardOut}\"')
+        elif not utils.isValidNum(sleepTimeSeconds):
+            raise ValueError(f'sleepTimeSeconds argument is malformed: \"{sleepTimeSeconds}\"')
+        elif sleepTimeSeconds < 0.1 or sleepTimeSeconds > 5:
+            raise ValueError(f'sleepTimeSeconds argument is out of bounds: {sleepTimeSeconds}')
         elif not utils.isValidStr(timberRootDirectory):
             raise ValueError(f'timberRootDirectory argument is malformed: \"{timberRootDirectory}\"')
 
         self.__alsoPrintToStandardOut: bool = alsoPrintToStandardOut
+        self.__sleepTimeSeconds: float = sleepTimeSeconds
         self.__timberRootDirectory: str = timberRootDirectory
+
+        self.__entryQueue: SimpleQueue[TimberEntry] = SimpleQueue()
+        eventLoop.create_task(self.__startEventLoop())
 
     def __getLogStatement(
         self,
@@ -49,10 +64,24 @@ class Timber():
             raise ValueError(f'msg argument is malformed: \"{msg}\"')
 
         timberEntry = TimberEntry(tag, msg)
+        self.__entryQueue.put(timberEntry)
+
+    def __log(self, timberEntry: TimberEntry):
+        if timberEntry is None:
+            raise ValueError(f'timberEntry argument is malformed: \"{timberEntry}\"')
+
         self.__writeToLogFile(timberEntry)
 
         if self.__alsoPrintToStandardOut:
             print(self.__getLogStatement(False, timberEntry))
+
+    async def __startEventLoop(self):
+        while True:
+            while not self.__entryQueue.empty():
+                timberEntry = self.__entryQueue.get()
+                self.__log(timberEntry)
+
+            asyncio.sleep(self.__sleepTimeSeconds)
 
     def __writeToLogFile(self, timberEntry: TimberEntry):
         if timberEntry is None:
