4b1654b93cfa819e9940feaa6490d3abebb20cae
Charles Madere <charlesrmadere@gmail.com>
2022-04-29 19:14:58 -0700

    TriviaFetchOptions now uses an enum for question/answer trivia questions

--- trivia/triviaFetchOptions.py
+++ trivia/triviaFetchOptions.py
@@ -1,7 +1,12 @@
 try:
     import CynanBotCommon.utils as utils
+    from CynanBotCommon.trivia.questionAnswerTriviaConditions import \
+        QuestionAnswerTriviaConditions
 except:
     import utils
+
+    from trivia.questionAnswerTriviaConditions import \
+        QuestionAnswerTriviaConditions
 
 
 class TriviaFetchOptions():
@@ -9,26 +14,23 @@ class TriviaFetchOptions():
     def __init__(
         self,
         twitchChannel: str,
-        areQuestionAnswerTriviaQuestionsEnabled: bool = False,
         isJokeTriviaRepositoryEnabled: bool = False,
-        requireQuestionAnswerTriviaQuestion: bool = False
+        questionAnswerTriviaConditions: QuestionAnswerTriviaConditions = QuestionAnswerTriviaConditions.NOT_ALLOWED
     ):
         if not utils.isValidStr(twitchChannel):
             raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
-        elif not utils.isValidBool(areQuestionAnswerTriviaQuestionsEnabled):
-            raise ValueError(f'areQuestionAnswerTriviaQuestionsEnabled argument is malformed: \"{areQuestionAnswerTriviaQuestionsEnabled}\"')
         elif not utils.isValidBool(isJokeTriviaRepositoryEnabled):
             raise ValueError(f'isJokeTriviaRepositoryEnabled argument is malformed: \"{isJokeTriviaRepositoryEnabled}\"')
-        elif not utils.isValidBool(requireQuestionAnswerTriviaQuestion):
-            raise ValueError(f'requireQuestionAnswerTriviaQuestion argument is malformed: \"{requireQuestionAnswerTriviaQuestion}\"')
+        elif questionAnswerTriviaConditions is None:
+            raise ValueError(f'questionAnswerTriviaConditions argument is malformed: \"{questionAnswerTriviaConditions}\"')
 
         self.__twitchChannel: str = twitchChannel
-        self.__areQuestionAnswerTriviaQuestionsEnabled: bool = areQuestionAnswerTriviaQuestionsEnabled
         self.__isJokeTriviaRepositoryEnabled: bool = isJokeTriviaRepositoryEnabled
-        self.__requireQuestionAnswerTriviaQuestion: bool = requireQuestionAnswerTriviaQuestion
+        self.__questionAnswerTriviaConditions: QuestionAnswerTriviaConditions = questionAnswerTriviaConditions
 
     def areQuestionAnswerTriviaQuestionsEnabled(self) -> bool:
-        return self.__areQuestionAnswerTriviaQuestionsEnabled
+        return self.__questionAnswerTriviaConditions is QuestionAnswerTriviaConditions.ALLOWED \
+            or self.__questionAnswerTriviaConditions is QuestionAnswerTriviaConditions.REQUIRED
 
     def getTwitchChannel(self) -> str:
         return self.__twitchChannel
@@ -37,4 +39,4 @@ class TriviaFetchOptions():
         return self.__isJokeTriviaRepositoryEnabled
 
     def requireQuestionAnswerTriviaQuestion(self) -> bool:
-        return self.__areQuestionAnswerTriviaQuestionsEnabled and self.__requireQuestionAnswerTriviaQuestion
+        return self.__questionAnswerTriviaConditions is QuestionAnswerTriviaConditions.REQUIRED
