6eb57a534c5c9fd1c68af88b81d126416b3667d7
Charles Madere <charlesrmadere@gmail.com>
2022-05-04 23:46:22 -0700

    TriviaGameMachine now reads in the levenshtein distance from TriviaSettingsRepository

--- trivia/triviaSettingsRepository.py
+++ trivia/triviaSettingsRepository.py
@@ -52,6 +52,10 @@ class TriviaSettingsRepository():
 
         return triviaSources
 
+    async def getLevenshteinThreshold(self) -> float:
+        jsonContents = await self.__readJson()
+        return utils.getFloatFromDict(jsonContents, 'levenshtein_threshold', 0.25)
+
     async def getMaxAnswerLength(self) -> int:
         jsonContents = await self.__readJson()
         return utils.getIntFromDict(jsonContents, 'max_answer_length', 80)
