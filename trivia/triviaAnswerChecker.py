d99e0d2aff39aec21ab0cc6f6afb00124254f819
Charles Madere <charlesrmadere@gmail.com>
2022-05-08 19:49:09 -0700

    moved trivia answer checking code into its own class

---
+++ trivia/triviaAnswerChecker.py
@@ -0,0 +1,136 @@
+import polyleven
+
+try:
+    import CynanBotCommon.utils as utils
+    from CynanBotCommon.timber.timber import Timber
+    from CynanBotCommon.trivia.absTriviaQuestion import AbsTriviaQuestion
+    from CynanBotCommon.trivia.multipleChoiceTriviaQuestion import \
+        MultipleChoiceTriviaQuestion
+    from CynanBotCommon.trivia.questionAnswerTriviaQuestion import \
+        QuestionAnswerTriviaQuestion
+    from CynanBotCommon.trivia.triviaAnswerCompiler import TriviaAnswerCompiler
+    from CynanBotCommon.trivia.triviaExceptions import (
+        BadTriviaAnswerException, UnsupportedTriviaTypeException)
+    from CynanBotCommon.trivia.triviaSettingsRepository import \
+        TriviaSettingsRepository
+    from CynanBotCommon.trivia.triviaType import TriviaType
+    from CynanBotCommon.trivia.trueFalseTriviaQuestion import \
+        TrueFalseTriviaQuestion
+except:
+    import utils
+    from timber.timber import Timber
+
+    from trivia.absTriviaQuestion import AbsTriviaQuestion
+    from trivia.multipleChoiceTriviaQuestion import \
+        MultipleChoiceTriviaQuestion
+    from trivia.questionAnswerTriviaQuestion import \
+        QuestionAnswerTriviaQuestion
+    from trivia.triviaAnswerCompiler import TriviaAnswerCompiler
+    from trivia.triviaExceptions import (BadTriviaAnswerException,
+                                         UnsupportedTriviaTypeException)
+    from trivia.triviaSettingsRepository import TriviaSettingsRepository
+    from trivia.triviaType import TriviaType
+    from trivia.trueFalseTriviaQuestion import TrueFalseTriviaQuestion
+
+
+class TriviaAnswerChecker():
+
+    def __init__(
+        self,
+        timber: Timber,
+        triviaAnswerCompiler: TriviaAnswerCompiler,
+        triviaSettingsRepository: TriviaSettingsRepository,
+    ):
+        if timber is None:
+            raise ValueError(f'timber argument is malformed: \"{timber}\"')
+        elif triviaAnswerCompiler is None:
+            raise ValueError(f'triviaAnswerCompiler argument is malformed: \"{triviaAnswerCompiler}\"')
+        elif triviaSettingsRepository is None:
+            raise ValueError(f'triviaSettingsRepository argument is malformed: \"{triviaSettingsRepository}\"')
+
+        self.__timber: Timber = timber
+        self.__triviaAnswerCompiler: TriviaAnswerCompiler = triviaAnswerCompiler
+        self.__triviaSettingsRepository: TriviaSettingsRepository = triviaSettingsRepository
+
+    async def checkAnswer(self, answer: str, triviaQuestion: AbsTriviaQuestion) -> bool:
+        if triviaQuestion is None:
+            raise ValueError(f'triviaQuestion argument is malformed: \"{triviaQuestion}\"')
+
+        if not utils.isValidStr(answer):
+            return False
+
+        if triviaQuestion.getTriviaType() is TriviaType.MULTIPLE_CHOICE:
+            return await self.__checkAnswerMultipleChoice(answer, triviaQuestion)
+        elif triviaQuestion.getTriviaType() is TriviaType.QUESTION_ANSWER:
+            return await self.__checkAnswerQuestionAnswer(answer, triviaQuestion)
+        elif triviaQuestion.getTriviaType() is TriviaType.TRUE_FALSE:
+            return await self.__checkAnswerTrueFalse(answer, triviaQuestion)
+        else:
+            raise UnsupportedTriviaTypeException(f'Unsupported TriviaType: \"{triviaQuestion.getTriviaType()}\"')
+
+    async def __checkAnswerMultipleChoice(
+        self,
+        answer: str,
+        triviaQuestion: MultipleChoiceTriviaQuestion
+    ) -> bool:
+        if triviaQuestion is None:
+            raise ValueError(f'triviaQuestion argument is malformed: \"{triviaQuestion}\"')
+        elif triviaQuestion.getTriviaType() is not TriviaType.MULTIPLE_CHOICE:
+            raise RuntimeError(f'TriviaType is not {TriviaType.MULTIPLE_CHOICE}: \"{triviaQuestion.getTriviaType()}\"')
+
+        answerOrdinal: int = None
+        try:
+            answerOrdinal = await self.__triviaAnswerCompiler.compileTextAnswerToMultipleChoiceOrdinal(answer)
+        except BadTriviaAnswerException as e:
+            self.__timber.log('TriviaAnswerChecker', f'Unable to convert multiple choice answer to ordinal: \"{answer}\": {e}')
+            return False
+
+        return answerOrdinal in triviaQuestion.getCorrectAnswerOrdinals()
+
+    async def __checkAnswerQuestionAnswer(
+        self,
+        answer: str,
+        triviaQuestion: QuestionAnswerTriviaQuestion
+    ) -> bool:
+        if triviaQuestion is None:
+            raise ValueError(f'triviaQuestion argument is malformed: \"{triviaQuestion}\"')
+        elif triviaQuestion.getTriviaType() is not TriviaType.QUESTION_ANSWER:
+            raise RuntimeError(f'TriviaType is not {TriviaType.QUESTION_ANSWER}: \"{triviaQuestion.getTriviaType()}\"')
+
+        cleanedAnswer = await self.__triviaAnswerCompiler.compileTextAnswer(answer)
+        if not utils.isValidStr(cleanedAnswer):
+            return False
+
+        correctAnswers = triviaQuestion.getCorrectAnswers()
+        cleanedCorrectAnswers = triviaQuestion.getCleanedCorrectAnswers()
+        levenshteinThreshold = await self.__triviaSettingsRepository.getLevenshteinThreshold()
+
+        for cleanedCorrectAnswer in cleanedCorrectAnswers:
+            threshold = int(min(len(cleanedAnswer), len(cleanedCorrectAnswer)) * levenshteinThreshold)
+            distance = polyleven.levenshtein(cleanedAnswer, cleanedCorrectAnswer, threshold)
+
+            self.__timber.log('TriviaAnswerChecker', f'answer:\"{answer}\", cleanedAnswer:\"{cleanedAnswer}\", correctAnswers:\"{correctAnswers}\", cleanedCorrectAnswers:\"{cleanedCorrectAnswers}\", threshold:\"{threshold}\", distance:\"{distance}\", levenshteinThreshold:\"{levenshteinThreshold}\"')
+
+            if distance <= threshold:
+                return True
+
+        return False
+
+    async def __checkAnswerTrueFalse(
+        self,
+        answer: str,
+        triviaQuestion: TrueFalseTriviaQuestion
+    ) -> bool:
+        if triviaQuestion is None:
+            raise ValueError(f'triviaQuestion argument is malformed: \"{triviaQuestion}\"')
+        elif triviaQuestion.getTriviaType() is not TriviaType.TRUE_FALSE:
+            raise RuntimeError(f'TriviaType is not {TriviaType.TRUE_FALSE}: \"{triviaQuestion.getTriviaType()}\"')
+
+        answerBool: bool = None
+        try:
+            answerBool = await self.__triviaAnswerCompiler.compileBoolAnswer(answer)
+        except BadTriviaAnswerException as e:
+            self.__timber.log('TriviaAnswerChecker', f'Unable to convert true false answer to bool: \"{answer}\": {e}')
+            return False
+
+        return answerBool in triviaQuestion.getCorrectAnswerBools()
