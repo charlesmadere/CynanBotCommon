0b24a9a7b46dbc12365a2db654279fa66fda0444
Charles Madere <charlesrmadere@gmail.com>
2022-05-13 20:02:53 -0700

    TriviaGameStore now uses different lists for normal games and super games

--- trivia/triviaGameStore.py
+++ trivia/triviaGameStore.py
@@ -21,44 +21,41 @@ except:
 class TriviaGameStore():
 
     def __init__(self):
-        self.__gameStates: List[AbsTriviaGameState] = list()
+        self.__normalGameStates: List[TriviaGameState] = list()
+        self.__superGameStates: List[SuperTriviaGameState] = list()
 
     async def add(self, state: AbsTriviaGameState):
         if state is None:
             raise ValueError(f'state argument is malformed: \"{state}\"')
 
-        self.__gameStates.append(state)
+        if state.getTriviaGameType() is TriviaGameType.NORMAL:
+            await self.__addNormalGame(state)
+        elif state.getTriviaGameType() is TriviaGameType.SUPER:
+            await self.__addSuperGame(state)
+        else:
+            raise UnknownTriviaGameTypeException(f'Unknown TriviaGameType: \"{state.getTriviaGameType()}\"')
 
-    async def __get(self, twitchChannel: str, userName: str, gameType: TriviaGameType) -> Optional[AbsTriviaGameState]:
-        if not utils.isValidStr(twitchChannel):
-            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
-        elif gameType is None:
-            raise ValueError(f'gameType argument is malformed: \"{gameType}\"')
+    async def __addNormalGame(self, state: TriviaGameState):
+        if state is None:
+            raise ValueError(f'state argument is malformed: \"{state}\"')
 
-        twitchChannel = twitchChannel.lower()
+        self.__normalGameStates.append(state)
 
-        if utils.isValidStr(userName):
-            userName = userName.lower()
+    async def __addSuperGame(self, state: SuperTriviaGameState):
+        if state is None:
+            raise ValueError(f'state argument is malformed: \"{state}\"')
 
-        gameStates = await self.getAll()
-
-        for state in gameStates:
-            if twitchChannel == state.getTwitchChannel().lower() and gameType is state.getTriviaGameType():
-                if gameType is TriviaGameType.NORMAL:
-                    normalGameState: TriviaGameState = state
-
-                    if userName == normalGameState.getUserName().lower():
-                        return normalGameState
-                elif gameType is TriviaGameType.SUPER:
-                    superGameState: SuperTriviaGameState = state
-                    return superGameState
-                else:
-                    raise UnknownTriviaGameTypeException(f'Unknown TriviaGameType (twitchChannel=\"{twitchChannel}\") (userName=\"{userName}\"): \"{state.getTriviaGameType()}\"')
-
-        return None
+        self.__superGameStates.append(state)
 
     async def getAll(self) -> List[AbsTriviaGameState]:
-        return utils.copyList(self.__gameStates)
+        normalGames = await self.getNormalGames()
+        superGames = await self.getSuperGames()
+
+        allGames: List[AbsTriviaGameState] = list()
+        allGames.extend(normalGames)
+        allGames.extend(superGames)
+
+        return allGames
 
     async def getNormalGame(self, twitchChannel: str, userName: str) -> Optional[TriviaGameState]:
         if not utils.isValidStr(twitchChannel):
@@ -66,48 +63,34 @@ class TriviaGameStore():
         elif not utils.isValidStr(userName):
             raise ValueError(f'userName argument is malformed: \"{userName}\"')
 
-        return await self.__get(
-            twitchChannel = twitchChannel,
-            userName = userName,
-            gameType = TriviaGameType.NORMAL
-        )
+        normalGames = await self.getNormalGames()
+        twitchChannel = twitchChannel.lower()
+        userName = userName.lower()
+
+        for state in normalGames:
+            if twitchChannel == state.getTwitchChannel().lower() and userName == state.getUserName().lower():
+                return state
+
+        return None
+
+    async def getNormalGames(self) -> List[TriviaGameState]:
+        return utils.copyList(self.__normalGameStates)
 
     async def getSuperGame(self, twitchChannel: str) -> Optional[SuperTriviaGameState]:
         if not utils.isValidStr(twitchChannel):
             raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
 
-        return await self.__get(
-            twitchChannel = twitchChannel,
-            userName = None,
-            gameType = TriviaGameType.SUPER
-        )
-
-    async def __remove(self, twitchChannel: str, userName: str, gameType: TriviaGameType) -> bool:
-        if not utils.isValidStr(twitchChannel):
-            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
-        elif gameType is None:
-            raise ValueError(f'gameType argument is malformed: \"{gameType}\"')
-
+        superGames = await self.getSuperGames()
         twitchChannel = twitchChannel.lower()
 
-        if utils.isValidStr(userName):
-            userName = userName.lower()
+        for state in superGames:
+            if twitchChannel == state.getTwitchChannel().lower():
+                return state
 
-        for state in self.__gameStates:
-            if twitchChannel == state.getTwitchChannel().lower() and gameType is state.getTriviaGameType():
-                if gameType is TriviaGameType.NORMAL:
-                    normalGameState: TriviaGameState = state
+        return None
 
-                    if userName == normalGameState.getUserName().lower():
-                        self.__gameStates.remove(state)
-                        return True
-                elif gameType is TriviaGameType.SUPER:
-                    self.__gameStates.remove(state)
-                    return True
-                else:
-                    raise UnknownTriviaGameTypeException(f'Unknown TriviaGameType (twitchChannel=\"{twitchChannel}\") (userName=\"{userName}\"): \"{state.getTriviaGameType()}\"')
-
-        return False
+    async def getSuperGames(self) -> List[SuperTriviaGameState]:
+        return utils.copyList(self.__superGameStates)
 
     async def removeNormalGame(self, twitchChannel: str, userName: str) -> bool:
         if not utils.isValidStr(twitchChannel):
@@ -115,18 +98,27 @@ class TriviaGameStore():
         elif not utils.isValidStr(userName):
             raise ValueError(f'userName argument is malformed: \"{userName}\"')
 
-        return await self.__remove(
-            twitchChannel = twitchChannel,
-            userName = userName,
-            gameType = TriviaGameType.NORMAL
-        )
+        normalGames = await self.getNormalGames()
+        twitchChannel = twitchChannel.lower()
+        userName = userName.lower()
+
+        for state in normalGames:
+            if twitchChannel == state.getTwitchChannel().lower() and userName == state.getUserName().lower():
+                self.__normalGameStates.remove(state)
+                return True
+
+        return False
 
     async def removeSuperGame(self, twitchChannel: str) -> bool:
         if not utils.isValidStr(twitchChannel):
             raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
 
-        return await self.__remove(
-            twitchChannel = twitchChannel,
-            userName = None,
-            gameType = TriviaGameType.SUPER
-        )
+        superGames = await self.getSuperGames()
+        twitchChannel = twitchChannel.lower()
+
+        for state in superGames:
+            if twitchChannel == state.getTwitchChannel().lower():
+                self.__superGameStates.remove(state)
+                return True
+
+        return False
