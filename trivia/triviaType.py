15fa12c4bf12bbf2aa2b0812219dbb7c38148c14
Charles Madere <charlesrmadere@gmail.com>
2022-06-13 16:32:43 -0700

    added some more string possibilities and a new toStr() method to TriviaType

--- trivia/triviaType.py
+++ trivia/triviaType.py
@@ -19,11 +19,21 @@ class TriviaType(Enum):
 
         text = text.lower()
 
-        if text == 'boolean' or text == 'true_false' or text == 'true false':
+        if text == 'boolean' or text == 'true-false' or text == 'true_false' or text == 'true false':
             return TriviaType.TRUE_FALSE
-        elif text == 'multiple' or text == 'multiple_choice' or text == 'multiple choice':
+        elif text == 'multiple' or text == 'multiple-choice' or text == 'multiple_choice' or text == 'multiple choice':
             return TriviaType.MULTIPLE_CHOICE
-        elif text == 'question_answer' or text == 'question answer':
+        elif text == 'question-answer' or text == 'question_answer' or text == 'question answer':
             return TriviaType.QUESTION_ANSWER
         else:
             raise ValueError(f'unknown TriviaType: \"{text}\"')
+
+    def toStr(self) -> str:
+        if self is TriviaType.MULTIPLE_CHOICE:
+            return 'multiple-choice'
+        elif self is TriviaType.QUESTION_ANSWER:
+            return 'question-answer'
+        elif self is TriviaType.TRUE_FALSE:
+            return 'true-false'
+        else:
+            raise ValueError(f'unknown TriviaType: \"{self}\"')
