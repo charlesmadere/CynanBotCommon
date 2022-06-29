11f1e487d822f8de330f1a8900a674b20de534b1
Charles Madere <charlesmadere@users.noreply.github.com>
2022-06-28 17:59:47 -0700

    testing an async test

--- trivia/tests/test_triviaAnswerCompiler.py
+++ trivia/tests/test_triviaAnswerCompiler.py
@@ -1,12 +1,29 @@
+import pytest
+
 try:
     from ...trivia.triviaAnswerCompiler import TriviaAnswerCompiler
+    from ...trivia.triviaExceptions import BadTriviaAnswerException
 except:
     from trivia.triviaAnswerCompiler import TriviaAnswerCompiler
+    from trivia.triviaExceptions import BadTriviaAnswerException
 
 
 class TestTriviaAnswerCompiler():
 
     __triviaAnswerCompiler: TriviaAnswerCompiler = TriviaAnswerCompiler()
+
+    @pytest.mark.asyncio
+    async def test_compileBoolAnswer_withNone(self):
+        exception: Exception = None
+        result: bool = None
+
+        try:
+            result = await self.__triviaAnswerCompiler.compileBoolAnswer(None)
+        except Exception as e:
+            exception = e
+
+        assert isinstance(exception, BadTriviaAnswerException)
+        assert result is None        
 
     def test_sanity(self):
         assert isinstance(self.__triviaAnswerCompiler, TriviaAnswerCompiler)
