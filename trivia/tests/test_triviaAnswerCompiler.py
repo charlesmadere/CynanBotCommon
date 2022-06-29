e8d798016a67b11d440f31f68c2977c1a94654b2
Charles Madere <charlesmadere@users.noreply.github.com>
2022-06-28 18:08:26 -0700

    added more tests

--- trivia/tests/test_triviaAnswerCompiler.py
+++ trivia/tests/test_triviaAnswerCompiler.py
@@ -13,6 +13,24 @@ class TestTriviaAnswerCompiler():
     __triviaAnswerCompiler: TriviaAnswerCompiler = TriviaAnswerCompiler()
 
     @pytest.mark.asyncio
+    async def test_compileBoolAnswer_withEmptyString(self):
+        exception: Exception = None
+        result: bool = None
+
+        try:
+            result = await self.__triviaAnswerCompiler.compileBoolAnswer('')
+        except Exception as e:
+            exception = e
+
+        assert isinstance(exception, BadTriviaAnswerException)
+        assert result is None
+
+    @pytest.mark.asyncio
+    async def test_compileBoolAnswer_withFalse(self):
+        result: bool = await self.__triviaAnswerCompiler.compileBoolAnswer('false')
+        assert result is False
+
+    @pytest.mark.asyncio
     async def test_compileBoolAnswer_withNone(self):
         exception: Exception = None
         result: bool = None
@@ -23,7 +41,25 @@ class TestTriviaAnswerCompiler():
             exception = e
 
         assert isinstance(exception, BadTriviaAnswerException)
-        assert result is None        
+        assert result is None
+
+    @pytest.mark.asyncio
+    async def test_compileBoolAnswer_withWhitespaceString(self):
+        exception: Exception = None
+        result: bool = None
+
+        try:
+            result = await self.__triviaAnswerCompiler.compileBoolAnswer(' ')
+        except Exception as e:
+            exception = e
+
+        assert isinstance(exception, BadTriviaAnswerException)
+        assert result is None
+
+    @pytest.mark.asyncio
+    async def test_compileBoolAnswer_withTrue(self):
+        result: bool = await self.__triviaAnswerCompiler.compileBoolAnswer('true')
+        assert result is True
 
     def test_sanity(self):
         assert isinstance(self.__triviaAnswerCompiler, TriviaAnswerCompiler)
