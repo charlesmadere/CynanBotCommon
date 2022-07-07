219073831c5fc06cbd465fd37cbab75054459c47
Charles Madere <charlesrmadere@gmail.com>
2022-07-07 16:58:19 -0700

    adjusted write intervals for ChatLogger and Timber

--- timber/timber.py
+++ timber/timber.py
@@ -19,7 +19,7 @@ class Timber():
         self,
         eventLoop: AbstractEventLoop,
         alsoPrintToStandardOut: bool = True,
-        sleepTimeSeconds: float = 0.25,
+        sleepTimeSeconds: float = 5,
         timberRootDirectory: str = 'CynanBotCommon/timber'
     ):
         if eventLoop is None:
@@ -28,7 +28,7 @@ class Timber():
             raise ValueError(f'alsoPrintToStandardOut argument is malformed: \"{alsoPrintToStandardOut}\"')
         elif not utils.isValidNum(sleepTimeSeconds):
             raise ValueError(f'sleepTimeSeconds argument is malformed: \"{sleepTimeSeconds}\"')
-        elif sleepTimeSeconds < 0.1 or sleepTimeSeconds > 5:
+        elif sleepTimeSeconds < 0.1 or sleepTimeSeconds > 60:
             raise ValueError(f'sleepTimeSeconds argument is out of bounds: {sleepTimeSeconds}')
         elif not utils.isValidStr(timberRootDirectory):
             raise ValueError(f'timberRootDirectory argument is malformed: \"{timberRootDirectory}\"')
