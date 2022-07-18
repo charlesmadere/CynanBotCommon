010c8d16c760c9b1461aa98f18e5bcae171aa77e
Charles Madere <charlesmadere@users.noreply.github.com>
2022-07-17 17:30:51 -0700

    added new BannedTriviaIdsRepository class

---
+++ trivia/bannedTriviaIdsRepository.py
@@ -0,0 +1,117 @@
+from aiosqlite import Connection
+
+try:
+    import CynanBotCommon.utils as utils
+    from CynanBotCommon.backingDatabase import BackingDatabase
+    from CynanBotCommon.timber.timber import Timber
+    from CynanBotCommon.trivia.triviaSettingsRepository import \
+        TriviaSettingsRepository
+    from CynanBotCommon.trivia.triviaSource import TriviaSource
+except:
+    import utils
+    from backingDatabase import BackingDatabase
+    from timber.timber import Timber
+
+    from trivia.triviaSettingsRepository import TriviaSettingsRepository
+    from trivia.triviaSource import TriviaSource
+
+
+class BannedTriviaIdsRepository():
+
+    def __init__(
+        self,
+        backingDatabase: BackingDatabase,
+        timber: Timber,
+        triviaSettingsRepository: TriviaSettingsRepository
+    ):
+        if backingDatabase is None:
+            raise ValueError(f'backingDatabase argument is malformed: \"{backingDatabase}\"')
+        elif timber is None:
+            raise ValueError(f'timber argument is malformed: \"{timber}\"')
+        elif triviaSettingsRepository is None:
+            raise ValueError(f'triviaSettingsRepository argument is malformed: \"{triviaSettingsRepository}\"')
+
+        self.__backingDatabase: BackingDatabase = backingDatabase
+        self.__timber: Timber = timber
+        self.__triviaSettingsRepository: TriviaSettingsRepository = triviaSettingsRepository
+
+        self.__isDatabaseReady: bool = False
+
+    async def ban(self, triviaSource: TriviaSource, triviaId: str):
+        if triviaSource is None:
+            raise ValueError(f'triviaSource argument is malformed: \"{triviaSource}\"')
+        elif not utils.isValidStr(triviaId):
+            raise ValueError(f'triviaId argument is malformed: \"{triviaId}\"')
+
+        if await self.__triviaSettingsRepository.isDebugLoggingEnabled():
+            self.__timber.log('BannedTriviaIdsRepository', f'Banning trivia question (triviaId=\"{triviaId}\", triviaSource=\"{triviaSource}\")...')
+
+        connection = await self.__getDatabaseConnection()
+        cursor = await connection.execute(
+            '''
+                INSERT OR IGNORE INTO bannedTriviaIds (triviaId, triviaSource)
+                VALUES (?, ?)
+            ''',
+            ( triviaId, triviaSource.toStr() )
+        )
+
+        await connection.commit()
+        await cursor.close()
+        await connection.close()
+
+    async def __getDatabaseConnection(self) -> Connection:
+        await self.__initDatabaseTable()
+        return await self.__backingDatabase.getConnection()
+
+    async def __initDatabaseTable(self):
+        if self.__isDatabaseReady:
+            return
+
+        self.__isDatabaseReady = True
+
+        connection = await self.__backingDatabase.getConnection()
+        cursor = await connection.execute(
+            '''
+                CREATE TABLE IF NOT EXISTS bannedTriviaIds (
+                    triviaId TEXT NOT NULL COLLATE NOCASE,
+                    triviaSource TEXT NOT NULL COLLATE NOCASE,
+                    PRIMARY KEY (triviaId, triviaSource)
+                )
+            '''
+        )
+
+        await connection.commit()
+        await cursor.close()
+        await connection.close()
+
+    async def isBanned(self, triviaSource: TriviaSource, triviaId: str) -> bool:
+        if triviaSource is None:
+            raise ValueError(f'triviaSource argument is malformed: \"{triviaSource}\"')
+        elif not utils.isValidStr(triviaId):
+            raise ValueError(f'triviaId argument is malformed: \"{triviaId}\"')
+
+        connection = await self.__getDatabaseConnection()
+        cursor = await connection.execute(
+            '''
+                SELECT COUNT(1) FROM bannedTriviaIds
+                WHERE triviaId = ? AND triviaSource = ?
+            ''',
+            ( triviaId, triviaSource.toStr() )
+        )
+
+        row = await cursor.fetchone()
+        count: int = None
+
+        if row is not None:
+            count = row[0]
+
+        await cursor.close()
+        await connection.close()
+
+        if not utils.isValidNum(count) or count < 1:
+            return False
+
+        if await self.__triviaSettingsRepository.isDebugLoggingEnabled():
+            self.__timber.log('BannedTriviaIdsRepository', f'Encountered banned trivia ID with count of {count} (triviaId=\"{triviaId}\", triviaSource=\"{triviaSource}\")')
+
+        return True
--- trivia/triviaVerifier.py
+++ trivia/triviaVerifier.py
@@ -1,6 +1,8 @@
 try:
     from CynanBotCommon.timber.timber import Timber
     from CynanBotCommon.trivia.absTriviaQuestion import AbsTriviaQuestion
+    from CynanBotCommon.trivia.bannedTriviaIdsRepository import \
+        BannedTriviaIdsRepository
     from CynanBotCommon.trivia.triviaContentCode import TriviaContentCode
     from CynanBotCommon.trivia.triviaContentScanner import TriviaContentScanner
     from CynanBotCommon.trivia.triviaFetchOptions import TriviaFetchOptions
@@ -11,6 +13,7 @@ except:
     from timber.timber import Timber
 
     from trivia.absTriviaQuestion import AbsTriviaQuestion
+    from trivia.bannedTriviaIdsRepository import BannedTriviaIdsRepository
     from trivia.triviaContentCode import TriviaContentCode
     from trivia.triviaContentScanner import TriviaContentScanner
     from trivia.triviaFetchOptions import TriviaFetchOptions
@@ -22,17 +25,21 @@ class TriviaVerifier():
 
     def __init__(
         self,
+        bannedTriviaIdsRepository: BannedTriviaIdsRepository,
         timber: Timber,
         triviaContentScanner: TriviaContentScanner,
         triviaHistoryRepository: TriviaHistoryRepository
     ):
-        if timber is None:
+        if bannedTriviaIdsRepository is None:
+            raise ValueError(f'bannedTriviaIdsRepository argument is malformed: \"bannedTriviaIdsRepository\"')
+        elif timber is None:
             raise ValueError(f'timber argument is malformed: \"{timber}\"')
         elif triviaContentScanner is None:
             raise ValueError(f'triviaContentScanner argument is malformed: \"{triviaContentScanner}\"')
         elif triviaHistoryRepository is None:
             raise ValueError(f'triviaHistoryRepository argument is malformed: \"{triviaHistoryRepository}\"')
 
+        self.__bannedTriviaIdsRepository: BannedTriviaIdsRepository = bannedTriviaIdsRepository
         self.__timber: Timber = timber
         self.__triviaContentScanner: TriviaContentScanner = triviaContentScanner
         self.__triviaHistoryRepository: TriviaHistoryRepository = triviaHistoryRepository
@@ -54,6 +61,9 @@ class TriviaVerifier():
         elif triviaFetchOptions.requireQuestionAnswerTriviaQuestion() and question.getTriviaType() is not TriviaType.QUESTION_ANSWER:
             self.__timber.log('TriviaVerifier', f'The given TriviaType is illegal: {question.getTriviaType()}')
             return TriviaContentCode.ILLEGAL_TRIVIA_TYPE
+
+        if await self.__bannedTriviaIdsRepository.isBanned(question.getTriviaSource(), question.getTriviaId()):
+            return TriviaContentCode.IS_BANNED
 
         contentScannerCode = await self.__triviaContentScanner.verify(question)
         if contentScannerCode is not TriviaContentCode.OK:
