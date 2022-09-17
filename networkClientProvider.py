de3fb46e59b07bfd594efa7a27912147a470d2ba
Charles Madere <charlesrmadere@gmail.com>
2022-09-17 11:53:51 -0700

    NetworkClientProvider now takes in an AbstractEventLoop

--- networkClientProvider.py
+++ networkClientProvider.py
@@ -1,3 +1,4 @@
+from asyncio import AbstractEventLoop
 from typing import Optional
 
 import aiohttp
@@ -12,19 +13,24 @@ class NetworkClientProvider():
 
     def __init__(
         self,
+        eventLoop: AbstractEventLoop,
         timeoutSeconds: int = 8
     ):
-        if not utils.isValidNum(timeoutSeconds):
+        if eventLoop is None:
+            raise ValueError(f'eventLoop argument is malformed: \"{eventLoop}\"')
+        elif not utils.isValidNum(timeoutSeconds):
             raise ValueError(f'timeoutSeconds argument is malformed: \"{timeoutSeconds}\"')
         elif timeoutSeconds < 2 or timeoutSeconds > 15:
             raise ValueError(f'timeoutSeconds argument is out of bounds: {timeoutSeconds}')
 
+        self.__eventLoop: AbstractEventLoop = eventLoop
         self.__timeoutSeconds: int = timeoutSeconds
         self.__clientSession: Optional[aiohttp.ClientSession] = None
 
     async def get(self) -> aiohttp.ClientSession:
         if self.__clientSession is None:
             self.__clientSession = aiohttp.ClientSession(
+                loop = self.__eventLoop,
                 cookie_jar = aiohttp.DummyCookieJar(),
                 timeout = aiohttp.ClientTimeout(total = self.__timeoutSeconds)
             )
