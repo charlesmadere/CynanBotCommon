f49bdb3819441de726238f48dd6ace87f32d19a9
Charles Madere <charlesmadere@users.noreply.github.com>
2022-09-24 14:34:26 -0700

    default to a max queue size of 25

--- trivia/triviaSettingsRepository.py
+++ trivia/triviaSettingsRepository.py
@@ -100,7 +100,7 @@ class TriviaSettingsRepository():
 
     async def getMaxSuperGameQueueSize(self) -> int:
         jsonContents = await self.__readJson()
-        maxSuperGameQueueSize = utils.getIntFromDict(jsonContents, 'max_super_game_queue_size', 0)
+        maxSuperGameQueueSize = utils.getIntFromDict(jsonContents, 'max_super_game_queue_size', 25)
 
         if maxSuperGameQueueSize < -1:
             raise ValueError(f'max_super_game_queue_size is too small: \"{maxSuperGameQueueSize}\"')
