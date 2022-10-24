0181d8dd16d72224a0323a5b8f0074f30fbbee2f
Charles Madere <charlesrmadere@gmail.com>
2022-10-24 00:35:07 -0700

    fixed a few issues and improved API in TriviaErrorDict

--- trivia/triviaErrorDict.py
+++ trivia/triviaErrorDict.py
@@ -15,13 +15,10 @@ class TriviaErrorDict():
             raise ValueError(f'fallOffTimeDelta argument is malformed: \"{fallOffTimeDelta}\"')
 
         self.__fallOffTimeDelta: timedelta = fallOffTimeDelta
-        self.__times: Dict[TriviaSource, datetime] = defaultdict(lambda: datetime(1970, 1, 1))
+        self.__times: Dict[TriviaSource, datetime] = dict()
         self.__values: Dict[TriviaSource, int] = defaultdict(lambda: 0)
 
     def __delitem__(self, key: TriviaSource):
-        if key is None or not isinstance(key, TriviaSource):
-            raise ValueError(f'key argument is malformed: \"{key}\"')
-
         raise RuntimeError(f'this method is not supported for TriviaErrorDict')
 
     def __getitem__(self, key: TriviaSource) -> int:
@@ -29,29 +26,31 @@ class TriviaErrorDict():
             raise ValueError(f'key argument is malformed: \"{key}\"')
 
         now = datetime.now(timezone.utc)
-        lastErrorTime = self.__times[key]
+        lastErrorTime = self.__times.get(key, None)
 
-        if now - lastErrorTime <= self.__fallOffTimeDelta:
+        if lastErrorTime is not None and now - lastErrorTime <= self.__fallOffTimeDelta:
             return self.__values[key]
 
         self.__values[key] = 0
         return 0
 
-    def incrementErrorCount(self, key: TriviaSource):
+    def incrementErrorCount(self, key: TriviaSource) -> int:
         if key is None or not isinstance(key, TriviaSource):
             raise ValueError(f'key argument is malformed: \"{key}\"')
 
         now = datetime.now(timezone.utc)
-        lastErrorTime = self.__times[key]
+        lastErrorTime = self.__times.get(key, None)
         self.__times[key] = now
+        newErrorCount: int = 0
 
-        if now - lastErrorTime <= self.__fallOffTimeDelta:
-            self.__values[key] = self.__values[key] + 1
+        if lastErrorTime is not None and now - lastErrorTime <= self.__fallOffTimeDelta:
+            newErrorCount = self.__values[key] + 1
+            self.__values[key] = newErrorCount
         else:
-            self.__values[key] = 1
+            newErrorCount = 1
+            self.__values[key] = newErrorCount
+
+        return newErrorCount
 
     def __setitem__(self, key: TriviaSource, value: int):
-        if key is None or not isinstance(key, TriviaSource):
-            raise ValueError(f'key argument is malformed: \"{key}\"')
-
         raise RuntimeError(f'this method is not supported for TriviaErrorDict')
