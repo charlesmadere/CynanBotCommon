a014864b3b1337331ee59b939df6ccac6d7277bb
Charles Madere <charlesrmadere@gmail.com>
2022-11-16 17:06:56 -0800

    DatabaseType can now be parsed from a string

--- storage/databaseType.py
+++ storage/databaseType.py
@@ -1,7 +1,26 @@
 from enum import Enum, auto
 
+try:
+    import CynanBotCommon.utils as utils
+except:
+    import utils
+
 
 class DatabaseType(Enum):
 
     POSTGRESQL = auto()
     SQLITE = auto()
+
+    @classmethod
+    def fromStr(cls, text: str):
+        if not utils.isValidStr(text):
+            raise ValueError(f'text argument is malformed: \"{text}\"')
+
+        text = text.lower()
+
+        if text == 'postgres' or text == 'postgresql':
+            return DatabaseType.POSTGRESQL
+        elif text == 'sqlite':
+            return DatabaseType.SQLITE
+        else:
+            raise RuntimeError(f'unknown DatabaseType: \"{text}\"')
