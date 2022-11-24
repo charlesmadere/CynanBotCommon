9d4adeaed8f19b59abb42b04278c2ca256e77cd1
Charles Madere <charlesrmadere@gmail.com>
2022-11-23 19:39:07 -0800

    tiny python cleanup

--- network/exceptions.py
+++ network/exceptions.py
@@ -1,10 +1,10 @@
-class NetworkResponseIsClosedException(Exception):
+class GenericNetworkException(Exception):
 
     def __init__(self, message: str):
         super().__init__(message)
 
 
-class GenericNetworkException(Exception):
+class NetworkResponseIsClosedException(Exception):
 
     def __init__(self, message: str):
         super().__init__(message)
