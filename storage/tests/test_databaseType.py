dd28639e14d978d52e109b19a4617210b499aa56
Charles Madere <charlesmadere@users.noreply.github.com>
2022-11-25 15:10:57 -0800

    added some basic storage and network tests

---
+++ storage/tests/test_databaseType.py
@@ -0,0 +1,58 @@
+try:
+    from ...storage.databaseType import DatabaseType
+except:
+    from storage.databaseType import DatabaseType
+
+
+class TestDatabaseType():
+
+    def test_fromStr_withEmptyString(self):
+        result: DatabaseType = None
+        exception: Exception = None
+
+        try:
+            result = DatabaseType.fromStr('')
+        except Exception as e:
+            exception = e
+
+        assert result is None
+        assert exception is not None
+        assert isinstance(exception, ValueError)
+
+    def test_fromStr_withPostgresString(self):
+        result = DatabaseType.fromStr('postgres')
+        assert result is DatabaseType.POSTGRESQL
+
+    def test_fromStr_withPostgresqlString(self):
+        result = DatabaseType.fromStr('postgresql')
+        assert result is DatabaseType.POSTGRESQL
+
+    def test_fromStr_withSqliteString(self):
+        result = DatabaseType.fromStr('sqlite')
+        assert result is DatabaseType.SQLITE
+
+    def test_fromStr_withNone(self):
+        result: DatabaseType = None
+        exception: Exception = None
+
+        try:
+            result = DatabaseType.fromStr(None)
+        except Exception as e:
+            exception = e
+
+        assert result is None
+        assert exception is not None
+        assert isinstance(exception, ValueError)
+
+    def test_fromStr_withWhitespaceString(self):
+        result: DatabaseType = None
+        exception: Exception = None
+
+        try:
+            result = DatabaseType.fromStr(' ')
+        except Exception as e:
+            exception = e
+
+        assert result is None
+        assert exception is not None
+        assert isinstance(exception, ValueError)
