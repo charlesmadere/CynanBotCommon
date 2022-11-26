3011de33afcbfd214ce32074e53f92994bccf62e
Charles Madere <charlesmadere@users.noreply.github.com>
2022-11-26 14:21:42 -0800

    TriviaSourceInstabilityHelper now logs its current error state situation

--- trivia/triviaSourceInstabilityHelper.py
+++ trivia/triviaSourceInstabilityHelper.py
@@ -3,18 +3,28 @@ from datetime import datetime, timedelta, timezone
 from typing import Dict, Optional
 
 try:
+    from CynanBotCommon.timber.timber import Timber
     from CynanBotCommon.trivia.triviaSource import TriviaSource
 except:
+    from timber.timber import Timber
     from trivia.triviaSource import TriviaSource
 
 
 class TriviaSourceInstabilityHelper():
 
-    def __init__(self, fallOffTimeDelta: timedelta = timedelta(hours = 1)):
-        if fallOffTimeDelta is None:
+    def __init__(
+        self,
+        timber: Timber,
+        fallOffTimeDelta: timedelta = timedelta(hours = 1)
+    ):
+        if timber is None:
+            raise ValueError(f'timber argument is malformed: \"{timber}\"')
+        elif fallOffTimeDelta is None:
             raise ValueError(f'fallOffTimeDelta argument is malformed: \"{fallOffTimeDelta}\"')
 
+        self.__timber: Timber = timber
         self.__fallOffTimeDelta: timedelta = fallOffTimeDelta
+
         self.__times: Dict[TriviaSource, Optional[datetime]] = dict()
         self.__values: Dict[TriviaSource, int] = defaultdict(lambda: 0)
 
@@ -47,4 +57,5 @@ class TriviaSourceInstabilityHelper():
             newErrorCount = 1
             self.__values[key] = newErrorCount
 
+        self.__timber.log('TriviaSourceInstabilityHelper', f'Incremented error count for {key} to {newErrorCount}')
         return newErrorCount
