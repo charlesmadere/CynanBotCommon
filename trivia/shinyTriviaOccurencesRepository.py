38bac85eb8006d173ba9c2a2f5f191dafe8e51e1
Charles Madere <charlesmadere@users.noreply.github.com>
2022-12-16 18:49:16 -0800

    finished filling in logic for ShinyTriviaOccurencesRepository?

--- trivia/shinyTriviaOccurencesRepository.py
+++ trivia/shinyTriviaOccurencesRepository.py
@@ -1,13 +1,23 @@
+from datetime import datetime, timezone
+from typing import Optional
+
 try:
+    import CynanBotCommon.utils as utils
     from CynanBotCommon.storage.backingDatabase import BackingDatabase
     from CynanBotCommon.storage.databaseConnection import DatabaseConnection
     from CynanBotCommon.storage.databaseType import DatabaseType
     from CynanBotCommon.timber.timber import Timber
+    from CynanBotCommon.trivia.shinyTriviaResult import ShinyTriviaResult
+    from CynanBotCommon.users.userIdsRepository import UserIdsRepository
 except:
+    import utils
     from storage.backingDatabase import BackingDatabase
     from storage.databaseConnection import DatabaseConnection
     from storage.databaseType import DatabaseType
     from timber.timber import Timber
+    from trivia.shinyTriviaResult import ShinyTriviaResult
+
+    from users.userIdsRepository import UserIdsRepository
 
 
 class ShinyTriviaOccurencesRepository():
@@ -15,21 +25,108 @@ class ShinyTriviaOccurencesRepository():
     def __init__(
         self,
         backingDatabase: BackingDatabase,
-        timber: Timber
+        timber: Timber,
+        userIdsRepository: UserIdsRepository,
+        timeZone: timezone = timezone.utc
     ):
         if not isinstance(backingDatabase, BackingDatabase):
             raise ValueError(f'backingDatabase argument is malformed: \"{backingDatabase}\"')
         elif not isinstance(timber, Timber):
             raise ValueError(f'timber argument is malformed: \"{timber}\"')
+        elif not isinstance(userIdsRepository, UserIdsRepository):
+            raise ValueError(f'userIdsRepository argument is malformed: \"{userIdsRepository}\"')
+        elif not isinstance(timeZone, timezone):
+            raise ValueError(f'timeZone argument is malformed: \"{timeZone}\"')
 
         self.__backingDatabase: BackingDatabase = backingDatabase
         self.__timber: Timber = timber
+        self.__userIdsRepository: UserIdsRepository = userIdsRepository
+        self.__timeZone: timezone = timeZone
 
         self.__isDatabaseReady: bool = False
+
+    async def fetchShinyCount(
+        self,
+        twitchChannel: str,
+        userId: str
+    ) -> ShinyTriviaResult:
+        if not utils.isValidStr(twitchChannel):
+            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
+        elif not utils.isValidStr(userId):
+            raise ValueError(f'userId argument is malformed: \"{userId}\"')
+        elif userId == '0':
+            raise ValueError(f'userId argument is an illegal value: \"{userId}\"')
+
+        userName = await self.__userIdsRepository.fetchUserName(userId)
+
+        connection = await self.__getDatabaseConnection()
+        record = await connection.fetchRow(
+            '''
+                SELECT count, mostrecent FROM shinytriviaoccurences
+                WHERE twitchchannel = $1 AND userid = $2
+                LIMIT 1
+            ''',
+            twitchChannel, userId
+        )
+
+        shinyCount: int = 0
+        mostRecent: Optional[datetime] = None
+
+        if utils.hasItems(record):
+            shinyCount = record[0]
+            mostRecent = utils.getDateTimeFromStr(record[1])
+
+        await connection.close()
+
+        return ShinyTriviaResult(
+            mostRecent = mostRecent,
+            newShinyCount = shinyCount,
+            oldShinyCount = shinyCount,
+            twitchChannel = twitchChannel,
+            userId = userId,
+            userName = userName
+        )
 
     async def __getDatabaseConnection(self) -> DatabaseConnection:
         await self.__initDatabaseTable()
         return await self.__backingDatabase.getConnection()
+
+    async def incrementShinyCount(
+        self,
+        twitchChannel: str,
+        userId: str
+    ) -> ShinyTriviaResult:
+        if not utils.isValidStr(twitchChannel):
+            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
+        elif not utils.isValidStr(userId):
+            raise ValueError(f'userId argument is malformed: \"{userId}\"')
+        elif userId == '0':
+            raise ValueError(f'userId argument is an illegal value: \"{userId}\"')
+
+        result = await self.fetchShinyCount(
+            twitchChannel = twitchChannel,
+            userId = userId
+        )
+
+        newShinyCount: int = result.getOldShinyCount() + 1
+
+        newResult = ShinyTriviaResult(
+            mostRecent = datetime.now(self.__timeZone),
+            newShinyCount = newShinyCount,
+            oldShinyCount = result.getOldShinyCount(),
+            twitchChannel = result.getTwitchChannel(),
+            userId = result.getUserId(),
+            userName = result.getUserName()
+        )
+
+        await self.__updateShinyCount(
+            newShinyCount = newResult.getNewShinyCount(),
+            twitchChannel = newResult.getTwitchChannel(),
+            userId = newResult.getUserId(),
+            userName = newResult.getUserName()
+        )
+
+        return newResult
 
     async def __initDatabaseTable(self):
         if self.__isDatabaseReady:
@@ -39,12 +136,65 @@ class ShinyTriviaOccurencesRepository():
         connection = await self.__backingDatabase.getConnection()
 
         if connection.getDatabaseType() is DatabaseType.POSTGRESQL:
-            # TODO
-            pass
+            await connection.createTableIfNotExists(
+                '''
+                    CREATE TABLE IF NOT EXISTS shinytriviaoccurences (
+                        count integer DEFAULT 0 NOT NULL,
+                        mostrecent text NOT NULL,
+                        twitchchannel public.citext NOT NULL,
+                        userid public.citext NOT NULL,
+                        PRIMARY KEY (twitchchannel, userid)
+                    )
+                '''
+            )
         elif connection.getDatabaseType() is DatabaseType.SQLITE:
-            # TODO
-            pass
+            await connection.createTableIfNotExists(
+                '''
+                    CREATE TABLE IF NOT EXISTS shinytriviaoccurences (
+                        count INTEGER NOT NULL DEFAULT 0,
+                        mostrecent TEXT NOT NULL,
+                        twitchchannel TEXT NOT NULL COLLATE NOCASE,
+                        userid TEXT NOT NULL COLLATE NOCASE,
+                        PRIMARY KEY (twitchchannel, userid)
+                    )
+                '''
+            )
         else:
             raise RuntimeError(f'Encountered unexpected DatabaseType when trying to create tables: \"{connection.getDatabaseType()}\"')
 
         await connection.close()
+
+    async def __updateShinyCount(
+        self,
+        newShinyCount: int,
+        twitchChannel: str,
+        userId: str,
+        userName: str
+    ):
+        if not utils.isValidInt(newShinyCount):
+            raise ValueError(f'newShinyCount argument is malformed: \"{newShinyCount}\"')
+        elif not utils.isValidStr(twitchChannel):
+            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
+        elif not utils.isValidStr(userId):
+            raise ValueError(f'userId argument is malformed: \"{userId}\"')
+        elif userId == '0':
+            raise ValueError(f'userId argument is an illegal value: \"{userId}\"')
+        elif not utils.isValidStr(userName):
+            raise ValueError(f'userName argument is malformed: \"{userName}\"')
+
+        nowDateTime = datetime.now(self.__timeZone)
+        nowDateTimeStr = nowDateTime.isoformat()
+
+        connection = await self.__getDatabaseConnection()
+        await connection.execute(
+            '''
+                    INSERT INTO shinytriviaoccurences (count, mostrecent, twitchchannel, userid)
+                    VALUES ($1, $2, $3, $4)
+                    ON CONFLICT (twitchchannel, userid) DO UPDATE SET count = EXCLUDED.count, mostrecent = EXCLUDED.mostrecent
+            ''',
+            newShinyCount, nowDateTimeStr, twitchChannel, userId
+        )
+
+        await connection.close()
+
+        self.__timber.log('ShinyTriviaOccurencesRepository', f'Shiny count for {userName}:{userId} in {twitchChannel} is now {newShinyCount}')
---
+++ trivia/shinyTriviaResult.py
@@ -0,0 +1,71 @@
+import locale
+from datetime import datetime
+from typing import Optional
+
+try:
+    import CynanBotCommon.utils as utils
+except:
+    import utils
+
+
+class ShinyTriviaResult():
+
+    def __init__(
+        self,
+        mostRecent: Optional[datetime],
+        newShinyCount: int,
+        oldShinyCount: int,
+        twitchChannel: str,
+        userId: str,
+        userName: str
+    ):
+        if mostRecent is not None and not isinstance(mostRecent, datetime):
+            raise ValueError(f'mostRecent argument is malformed: \"{mostRecent}\"')
+        elif not utils.isValidInt(newShinyCount):
+            raise ValueError(f'newShinyCount argument is malformed: \"{newShinyCount}\"')
+        elif newShinyCount < 0:
+            raise ValueError(f'newShinyCount argument is out of bounds: {newShinyCount}')
+        elif not utils.isValidInt(oldShinyCount):
+            raise ValueError(f'oldShinyCount argument is malformed: \"{oldShinyCount}\"')
+        elif oldShinyCount < 0:
+            raise ValueError(f'oldShinyCount argument is out of bounds: {oldShinyCount}')
+        elif not utils.isValidStr(twitchChannel):
+            raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
+        elif not utils.isValidStr(userId):
+            raise ValueError(f'userId argument is malformed: \"{userId}\"')
+        elif not utils.isValidStr(userName):
+            raise ValueError(f'userName argument is malformed: \"{userName}\"')
+
+        self.__mostRecent: Optional[datetime] = mostRecent
+        self.__newShinyCount: int = newShinyCount
+        self.__oldShinyCount: int = oldShinyCount
+        self.__twitchChannel: str = twitchChannel
+        self.__userId: str = userId
+        self.__userName: str = userName
+
+    def getMostRecent(self) -> Optional[datetime]:
+        return self.__mostRecent
+
+    def getNewShinyCount(self) -> int:
+        return self.__newShinyCount
+
+    def getNewShinyCountStr(self) -> str:
+        return locale.format_string("%d", self.__newShinyCount, grouping = True)
+
+    def getOldShinyCount(self) -> int:
+        return self.__oldShinyCount
+
+    def getOldShinyCountStr(self) -> str:
+        return locale.format_string("%d", self.__oldShinyCount, grouping = True)
+
+    def getTwitchChannel(self) -> str:
+        return self.__twitchChannel
+
+    def getUserId(self) -> str:
+        return self.__userId
+
+    def getUserName(self) -> str:
+        return self.__userName
+
+    def hasMostRecent(self) -> bool:
+        return self.__mostRecent is not None
