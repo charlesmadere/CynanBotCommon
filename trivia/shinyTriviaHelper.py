9044a5b644eda85ef024073af20bd18d11a7040d
Charles Madere <charlesmadere@users.noreply.github.com>
2022-12-20 16:31:52 -0800

    ShinyTriviaHelper now also utilizes the user’s most recent shiny datetime

--- trivia/shinyTriviaHelper.py
+++ trivia/shinyTriviaHelper.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime, timedelta, timezone
 from typing import Dict, Optional
 
 try:
@@ -25,7 +26,9 @@ class ShinyTriviaHelper():
         cutenessRepository: CutenessRepository,
         shinyTriviaOccurencesRepository: ShinyTriviaOccurencesRepository,
         timber: Timber,
-        triviaSettingsRepository: TriviaSettingsRepository
+        triviaSettingsRepository: TriviaSettingsRepository,
+        cooldown: timedelta = timedelta(hours = 18),
+        timeZone: timezone = timezone.utc
     ):
         if not isinstance(cutenessRepository, CutenessRepository):
             raise ValueError(f'cutenessRepository argument is malformed: \"{cutenessRepository}\"')
@@ -35,11 +38,17 @@ class ShinyTriviaHelper():
             raise ValueError(f'timber argument is malformed: \"{timber}\"')
         elif not isinstance(triviaSettingsRepository, TriviaSettingsRepository):
             raise ValueError(f'triviaSettingsRepository argument is malformed: \"{triviaSettingsRepository}\"')
+        elif not isinstance(cooldown, timedelta):
+            raise ValueError(f'cooldown argument is malformed: \"{cooldown}\"')
+        elif not isinstance(timeZone, timezone):
+            raise ValueError(f'timeZone argument is malformed: \"{timeZone}\"')
 
         self.__cutenessRepository: CutenessRepository = cutenessRepository
         self.__shinyTriviaOccurencesRepository: ShinyTriviaOccurencesRepository = shinyTriviaOccurencesRepository
         self.__timber: Timber = timber
         self.__triviaSettingsRepository: TriviaSettingsRepository = triviaSettingsRepository
+        self.__cooldown: timedelta = cooldown
+        self.__timeZone: timezone = timeZone
 
         self.__rankToProbabilityDict: Dict[int, float] = self.__createRankToProbabilityDict()
 
@@ -108,11 +117,23 @@ class ShinyTriviaHelper():
         if random.uniform(0, 1) >= probability:
             return False
 
-        shinyResult = await self.__shinyTriviaOccurencesRepository.incrementShinyCount(
+        details = await self.__shinyTriviaOccurencesRepository.fetchDetails(
             twitchChannel = twitchChannel,
             userId = userId
         )
 
-        self.__timber.log('ShinyTriviaHelper', f'{shinyResult.getUserName()}:{shinyResult.getUserId()} has encountered a shiny!')
+        if details.getMostRecent() is not None:
+            now = datetime.now(self.__timeZone)
+
+            if now - details.getMostRecent() < self.__cooldown:
+                self.__timber.log('ShinyTriviaHelper', f'{details.getUserName()}:{details.getUserId()} in {details.getTwitchChannel()} would have encountered a shiny, but it was rejected, as their most recent shiny ({details.getMostRecent()}) is within the cooldown time')
+                return False
+
+        result = await self.__shinyTriviaOccurencesRepository.incrementShinyCount(
+            twitchChannel = twitchChannel,
+            userId = userId
+        )
+
+        self.__timber.log('ShinyTriviaHelper', f'{result.getUserName()}:{result.getUserId()} in {result.getTwitchChannel()} has encountered a shiny!')
 
         return True
