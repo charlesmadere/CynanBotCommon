1a8d55e0464ecf547ff9c04b23db885f609afb4b
Charles Madere <charlesmadere@users.noreply.github.com>
2023-01-27 11:03:33 -0800

    fixed dumb bug

--- cuteness/cutenessRepository.py
+++ cuteness/cutenessRepository.py
@@ -327,7 +327,7 @@ class CutenessRepository():
     ) -> CutenessResult:
         if not utils.isValidInt(incrementAmount):
             raise ValueError(f'incrementAmount argument is malformed: \"{incrementAmount}\"')
-        elif incrementAmount < utils.getLongMaxSafeSize() or incrementAmount > utils.getLongMaxSafeSize():
+        elif incrementAmount < utils.getLongMinSafeSize() or incrementAmount > utils.getLongMaxSafeSize():
             raise ValueError(f'incrementAmount argument is out of bounds: {incrementAmount}')
         elif not utils.isValidStr(twitchChannel):
             raise ValueError(f'twitchChannel argument is malformed: \"{twitchChannel}\"')
