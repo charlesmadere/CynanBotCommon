ac39d5abd8946faab340746383c0fef744f62bbf
Charles Madere <charlesrmadere@gmail.com>
2023-07-12 22:16:58 +0900

    further skeleton progress

--- recurringActions/recurringActionsHelper.py
+++ recurringActions/recurringActionsHelper.py
@@ -5,16 +5,31 @@ try:
         RecurringActionListener
     from CynanBotCommon.recurringActions.recurringActionsHelperInterface import \
         RecurringActionsHelperInterface
+    from CynanBotCommon.storage.backingDatabase import BackingDatabase
+    from CynanBotCommon.storage.databaseConnection import DatabaseConnection
+    from CynanBotCommon.storage.databaseType import DatabaseType
 except:
     from recurringActions.recurringActionListener import \
         RecurringActionListener
     from recurringActions.recurringActionsHelperInterface import \
         RecurringActionsHelperInterface
+    from storage.backingDatabase import BackingDatabase
+    from storage.databaseConnection import DatabaseConnection
+    from storage.databaseType import DatabaseType
 
 
 class RecurringActionsHelper(RecurringActionsHelperInterface):
 
-    def __init__(self):
+    def __init__(
+        self,
+        backingDatabase: BackingDatabase
+    ):
+        if not isinstance(backingDatabase, BackingDatabase):
+            raise ValueError(f'backingDatabase argument is malformed: \"{backingDatabase}\"')
+
+        self.__backingDatabase: BackingDatabase = backingDatabase
+
+        self.__isDatabaseReady: bool = False
         self.__actionListener: Optional[RecurringActionListener] = None
 
     def setRecurringActionListener(self, listener: Optional[RecurringActionListener]):
