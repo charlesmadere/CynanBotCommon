3f04754af2f7013216b3663b1bb498bfbed3e484
Charles Madere <charlesrmadere@gmail.com>
2023-07-18 23:16:41 +0900

    added abstract python typing info to TriviaEventListener

--- trivia/triviaEventListener.py
+++ trivia/triviaEventListener.py
@@ -1,10 +1,13 @@
+from abc import ABC, abstractmethod
+
 try:
     from CynanBotCommon.trivia.absTriviaEvent import AbsTriviaEvent
 except:
     from trivia.absTriviaEvent import AbsTriviaEvent
 
 
-class TriviaEventListener():
+class TriviaEventListener(ABC):
 
+    @abstractmethod
     async def onNewTriviaEvent(self, event: AbsTriviaEvent):
         pass
