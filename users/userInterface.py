1bc4a48565435cb7dab72accee9a8c6fece7165e
Charles Madere <charlesrmadere@gmail.com>
2023-07-30 21:21:04 +0900

    working on building a new way to create trivia games

--- users/userInterface.py
+++ users/userInterface.py
@@ -14,3 +14,19 @@ class UserInterface(ABC):
     @abstractmethod
     def isEnabled(self) -> bool:
         pass
+
+    @abstractmethod
+    def isShinyTriviaEnabled(self) -> bool:
+        pass
+
+    @abstractmethod
+    def isSuperTriviaGameEnabled(self) -> bool:
+        pass
+
+    @abstractmethod
+    def isToxicTriviaEnabled(self) -> bool:
+        pass
+
+    @abstractmethod
+    def isTriviaGameEnabled(self) -> bool:
+        pass
