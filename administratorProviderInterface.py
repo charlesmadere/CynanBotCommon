392155b02fa83cdb487612dbc06fcba825c88873
Charles Madere <charlesrmadere@gmail.com>
2023-08-03 00:25:23 +0900

    interface improvement

--- administratorProviderInterface.py
+++ administratorProviderInterface.py
@@ -1,7 +1,12 @@
-class AdministratorProviderInterface():
+from abc import ABC, abstractmethod
 
+
+class AdministratorProviderInterface(ABC):
+
+    @abstractmethod
     async def getAdministratorUserId(self) -> str:
         pass
 
+    @abstractmethod
     async def getAdministratorUserName(self) -> str:
         pass
