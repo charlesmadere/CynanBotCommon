c9c5afc08946124316375670e94c3546a3e84b90
Charles Madere <charlesrmadere@gmail.com>
2023-08-06 17:43:47 +0900

    added abstract class notations to TwitchTokensRepositoryInterface

--- twitch/twitchTokensRepositoryInterface.py
+++ twitch/twitchTokensRepositoryInterface.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import List, Optional
 
 try:
@@ -10,40 +11,52 @@ except:
         TwitchTokensRepositoryListener
 
 
-class TwitchTokensRepositoryInterface():
+class TwitchTokensRepositoryInterface(ABC):
 
+    @abstractmethod
     async def addUser(self, code: str, twitchChannel: str):
         pass
 
+    @abstractmethod
     async def clearCaches(self):
         pass
 
+    @abstractmethod
     async def getAccessToken(self, twitchChannel: str) -> Optional[str]:
         pass
 
+    @abstractmethod
     async def getExpiringTwitchChannels(self) -> Optional[List[str]]:
         pass
 
+    @abstractmethod
     async def getRefreshToken(self, twitchChannel: str) -> Optional[str]:
         pass
 
+    @abstractmethod
     async def hasAccessToken(self, twitchChannel: str) -> bool:
         pass
 
+    @abstractmethod
     async def removeUser(self, twitchChannel: str):
         pass
 
+    @abstractmethod
     async def requireAccessToken(self, twitchChannel: str) -> str:
         pass
 
+    @abstractmethod
     async def requireRefreshToken(self, twitchChannel: str) -> str:
         pass
 
+    @abstractmethod
     async def requireTokensDetails(self, twitchChannel: str) -> TwitchTokensDetails:
         pass
 
+    @abstractmethod
     def setListener(self, listener: Optional[TwitchTokensRepositoryListener]):
         pass
 
+    @abstractmethod
     async def validateAndRefreshAccessToken(self, twitchChannel: str):
         pass
