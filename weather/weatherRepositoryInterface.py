0cd31f5e858e8de44d8804c2540bad83b22a5b2f
Charles Madere <charlesrmadere@gmail.com>
2023-09-18 20:39:18 +0900

    beginning work on new `Clearable` interface class

--- weather/weatherRepositoryInterface.py
+++ weather/weatherRepositoryInterface.py
@@ -1,18 +1,16 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 
 try:
+    from CynanBotCommon.clearable import Clearable
     from CynanBotCommon.location.location import Location
     from CynanBotCommon.weather.weatherReport import WeatherReport
 except:
+    from clearable import Clearable
     from location.location import Location
     from weather.weatherReport import WeatherReport
 
 
-class WeatherRepositoryInterface(ABC):
-
-    @abstractmethod
-    async def clearCaches(self):
-        pass
+class WeatherRepositoryInterface(Clearable):
 
     @abstractmethod
     async def fetchWeather(self, location: Location) -> WeatherReport:
