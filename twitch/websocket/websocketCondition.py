6b3a28c1062476f613f6301558c14f5f6cfc855b
Charles Madere <charlesrmadere@gmail.com>
2023-10-09 14:36:42 +0900

    add a ton more fields to `WebsocketCondition` class

--- twitch/websocket/websocketCondition.py
+++ twitch/websocket/websocketCondition.py
@@ -1,19 +1,164 @@
+from typing import Optional
+
 try:
     import CynanBotCommon.utils as utils
+    from CynanBotCommon.twitch.twitchSubscriberTier import TwitchSubscriberTier
 except:
     import utils
+
+    from twitch.twitchSubscriberTier import TwitchSubscriberTier
 
 
 class WebsocketCondition():
 
     def __init__(
         self,
-        broadcasterUserId: str
+        isAnonymous: Optional[bool],
+        isGift: Optional[bool],
+        bits: Optional[int],
+        viewers: Optional[int],
+        broadcasterUserId: Optional[str],
+        broadcasterUserLogin: Optional[str],
+        broadcasterUserName: Optional[str],
+        clientId: Optional[str],
+        fromBroadcasterUserId: Optional[str],
+        fromBroadcasterUserLogin: Optional[str],
+        fromBroadcasterUserName: Optional[str],
+        message: Optional[str],
+        moderatorUserId: Optional[str],
+        rewardId: Optional[str],
+        toBroadcasterUserId: Optional[str],
+        toBroadcasterUserLogin: Optional[str],
+        toBroadcasterUserName: Optional[str],
+        userId: Optional[str],
+        userLogin: Optional[str],
+        userName: Optional[str],
+        tier: Optional[TwitchSubscriberTier]
     ):
-        if not utils.isValidStr(broadcasterUserId):
+        if isAnonymous is not None and not utils.isValidBool(isAnonymous):
+            raise ValueError(f'isAnonymous argument is malformed: \"{isAnonymous}\"')
+        elif isGift is not None and not utils.isValidBool(isGift):
+            raise ValueError(f'isGift argument is malformed: \"{isGift}\"')
+        elif bits is not None and not utils.isValidInt(bits):
+            raise ValueError(f'bits argument is malformed: \"{bits}\"')
+        elif viewers is not None and not utils.isValidInt(viewers):
+            raise ValueError(f'viewers argument is malformed: \"{viewers}\"')
+        if broadcasterUserId is not None and not utils.isValidStr(broadcasterUserId):
             raise ValueError(f'broadcasterUserId argument is malformed: \"{broadcasterUserId}\"')
+        if broadcasterUserLogin is not None and not utils.isValidStr(broadcasterUserLogin):
+            raise ValueError(f'broadcasterUserLogin argument is malformed: \"{broadcasterUserLogin}\"')
+        if broadcasterUserName is not None and not utils.isValidStr(broadcasterUserName):
+            raise ValueError(f'broadcasterUserName argument is malformed: \"{broadcasterUserName}\"')
+        elif clientId is not None and not utils.isValidStr(clientId):
+            raise ValueError(f'clientId argument is malformed: \"{clientId}\"')
+        elif fromBroadcasterUserId is not None and not utils.isValidStr(fromBroadcasterUserId):
+            raise ValueError(f'fromBroadcasterUserId argument is malformed: \"{fromBroadcasterUserId}\"')
+        elif fromBroadcasterUserLogin is not None and not utils.isValidStr(fromBroadcasterUserLogin):
+            raise ValueError(f'fromBroadcasterUserLogin argument is malformed: \"{fromBroadcasterUserLogin}\"')
+        elif fromBroadcasterUserName is not None and not utils.isValidStr(fromBroadcasterUserName):
+            raise ValueError(f'fromBroadcasterUserName argument is malformed: \"{fromBroadcasterUserName}\"')
+        elif message is not None and not utils.isValidStr(message):
+            raise ValueError(f'message argument is malformed: \"{message}\"')
+        elif moderatorUserId is not None and not utils.isValidStr(moderatorUserId):
+            raise ValueError(f'moderatorUserId argument is malformed: \"{moderatorUserId}\"')
+        elif rewardId is not None and not utils.isValidStr(rewardId):
+            raise ValueError(f'rewardId argument is malformed: \"{rewardId}\"')
+        elif toBroadcasterUserId is not None and not utils.isValidStr(toBroadcasterUserId):
+            raise ValueError(f'toBroadcasterUserId argument is malformed: \"{toBroadcasterUserId}\"')
+        elif toBroadcasterUserLogin is not None and not utils.isValidStr(toBroadcasterUserLogin):
+            raise ValueError(f'toBroadcasterUserLogin argument is malformed: \"{toBroadcasterUserLogin}\"')
+        elif toBroadcasterUserName is not None and not utils.isValidStr(toBroadcasterUserName):
+            raise ValueError(f'toBroadcasterUserName argument is malformed: \"{toBroadcasterUserName}\"')
+        elif userId is not None and not utils.isValidStr(userId):
+            raise ValueError(f'userId argument is malformed: \"{userId}\"')
+        elif userLogin is not None and not utils.isValidStr(userLogin):
+            raise ValueError(f'userLogin argument is malformed: \"{userLogin}\"')
+        elif userName is not None and not utils.isValidStr(userName):
+            raise ValueError(f'userName argument is malformed: \"{userName}\"')
+        elif tier is not None and not isinstance(tier, TwitchSubscriberTier):
+            raise ValueError(f'tier argument is malformed: \"{tier}\"')
 
-        self.__broadcasterUserId: str = broadcasterUserId
+        self.__isAnonymous: Optional[bool] = isAnonymous
+        self.__isGift: Optional[bool] = isGift
+        self.__bits: Optional[int] = bits
+        self.__viewers: Optional[int] = viewers
+        self.__broadcasterUserId: Optional[str] = broadcasterUserId
+        self.__broadcasterUserLogin: Optional[str] = broadcasterUserLogin
+        self.__broadcasterUserName: Optional[str] = broadcasterUserName
+        self.__clientId: Optional[str] = clientId
+        self.__fromBroadcasterUserId: Optional[str] = fromBroadcasterUserId
+        self.__fromBroadcasterUserLogin: Optional[str] = fromBroadcasterUserLogin
+        self.__fromBroadcasterUserName: Optional[str] = fromBroadcasterUserName
+        self.__message: Optional[str] = message
+        self.__moderatorUserId: Optional[str] = moderatorUserId
+        self.__rewardId: Optional[str] = rewardId
+        self.__toBroadcasterUserId: Optional[str] = toBroadcasterUserId
+        self.__toBroadcasterUserLogin: Optional[str] = toBroadcasterUserLogin
+        self.__toBroadcasterUserName: Optional[str] = toBroadcasterUserName
+        self.__userId: Optional[str] = userId
+        self.__userLogin: Optional[str] = userLogin
+        self.__userName: Optional[str] = userName
+        self.__tier: Optional[TwitchSubscriberTier] = tier
 
-    def getBroadcasterUserId(self) -> str:
+    def getBits(self) -> Optional[int]:
+        return self.__bits
+
+    def getBroadcasterUserId(self) -> Optional[str]:
         return self.__broadcasterUserId
+
+    def getBroadcasterUserLogin(self) -> Optional[str]:
+        return self.__broadcasterUserLogin
+
+    def getBroadcasterUserName(self) -> Optional[str]:
+        return self.__broadcasterUserName
+
+    def getClientId(self) -> Optional[str]:
+        return self.__clientId
+
+    def getFromBroadcasterUserId(self) -> Optional[str]:
+        return self.__fromBroadcasterUserId
+
+    def getFromBroadcasterUserLogin(self) -> Optional[str]:
+        return self.__fromBroadcasterUserLogin
+
+    def getFromBroadcasterUserName(self) -> Optional[str]:
+        return self.__fromBroadcasterUserName
+
+    def getMessage(self) -> Optional[str]:
+        return self.__message
+
+    def getModeratorUserId(self) -> Optional[str]:
+        return self.__moderatorUserId
+
+    def getRewardId(self) -> Optional[str]:
+        return self.__rewardId
+
+    def getTier(self) -> Optional[TwitchSubscriberTier]:
+        return self.__tier
+
+    def getToBroadcasterUserId(self) -> Optional[str]:
+        return self.__toBroadcasterUserId
+
+    def getToBroadcasterUserLogin(self) -> Optional[str]:
+        return self.__toBroadcasterUserLogin
+
+    def getToBroadcasterUserName(self) -> Optional[str]:
+        return self.__toBroadcasterUserName
+
+    def getUserId(self) -> Optional[str]:
+        return self.__userId
+
+    def getUserLogin(self) -> Optional[str]:
+        return self.__userLogin
+
+    def getUserName(self) -> Optional[str]:
+        return self.__userName
+
+    def getViewers(self) -> Optional[int]:
+        return self.__viewers
+
+    def isAnonymous(self) -> Optional[bool]:
+        return self.__isAnonymous
+
+    def isGift(self) -> Optional[bool]:
+        return self.__isGift
