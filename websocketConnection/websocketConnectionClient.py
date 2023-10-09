c2b640532238e221cc47c3f0112643a06840f8e4
Charles Madere <charlesrmadere@gmail.com>
2023-10-09 14:37:35 +0900

    removed unused classes

--- websocketConnection/websocketConnectionClient.py
+++
@@ -1,9 +0,0 @@
-try:
-    from CynanBotCommon.websocketConnection.websocketConnectionClientInterface import WebsocketConnectionClientInterface
-except:
-    from websocketConnection.websocketConnectionClientInterface import WebsocketConnectionClientInterface
-
-
-class WebsocketConnectionClient(WebsocketConnectionClientInterface):
-
-    pass
