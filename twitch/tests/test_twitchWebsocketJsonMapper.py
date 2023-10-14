0ad71fb22c70b660203fecbc3a0ea31cdc987f48
Charles Madere <charlesrmadere@gmail.com>
2023-10-14 21:17:59 +0900

    fixed a unit test

--- twitch/tests/test_twitchWebsocketJsonMapper.py
+++ twitch/tests/test_twitchWebsocketJsonMapper.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 
 try:
@@ -6,6 +8,7 @@ try:
     from ..websocket.twitchWebsocketJsonMapper import TwitchWebsocketJsonMapper
     from ..websocket.twitchWebsocketJsonMapperInterface import \
         TwitchWebsocketJsonMapperInterface
+    from ..websocket.websocketCondition import WebsocketCondition
 except:
     from timber.timberInterface import TimberInterface
     from timber.timberStub import TimberStub
@@ -14,6 +17,7 @@ except:
         TwitchWebsocketJsonMapper
     from twitch.websocket.twitchWebsocketJsonMapperInterface import \
         TwitchWebsocketJsonMapperInterface
+    from twitch.websocket.websocketCondition import WebsocketCondition
 
 
 class TestTwitchWebsocketJsonMapper():
@@ -31,7 +35,48 @@ class TestTwitchWebsocketJsonMapper():
     @pytest.mark.asyncio
     async def test_parseWebsocketCondition_withEmptyDictionary(self):
         result = await self.jsonMapper.parseWebsocketCondition(dict())
-        assert result is None
+        assert isinstance(result, WebsocketCondition)
+        assert result.getBits() is None
+        assert result.getBroadcasterUserId() is None
+        assert result.getBroadcasterUserLogin() is None
+        assert result.getBroadcasterUserName() is None
+        assert result.getCategoryId() is None
+        assert result.getCategoryName() is None
+        assert result.getClientId() is None
+        assert result.getCumulativeTotal() is None
+        assert result.getFromBroadcasterUserId() is None
+        assert result.getFromBroadcasterUserLogin() is None
+        assert result.getFromBroadcasterUserName() is None
+        assert result.getMessage() is None
+        assert result.getModeratorUserId() is None
+        assert result.getModeratorUserLogin() is None
+        assert result.getModeratorUserName() is None
+        assert result.getReason() is None
+        assert result.getRewardId() is None
+        assert result.getTier() is None
+        assert result.getTitle() is None
+        assert result.getToBroadcasterUserId() is None
+        assert result.getToBroadcasterUserLogin() is None
+        assert result.getToBroadcasterUserName() is None
+        assert result.getTotal() is None
+        assert result.getUserId() is None
+        assert result.getUserLogin() is None
+        assert result.getUserName() is None
+        assert result.getViewers() is None
+        assert result.isAnonymous() is None
+        assert result.isGift() is None
+        assert result.isPermanent() is None
+
+        broadcasterUserId: Optional[str] = None
+        exception: Optional[Exception] = None
+
+        try:
+            broadcasterUserId = result.requireBroadcasterUserId()
+        except Exception as e:
+            exception = e
+
+        assert broadcasterUserId is None
+        assert isinstance(exception, Exception)
 
     @pytest.mark.asyncio
     async def test_parseWebsocketCondition_withNone(self):
