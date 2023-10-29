62672a26298b1ad7563fc29f49b5446b86fcbf94
Charles Madere <charlesrmadere@gmail.com>
2023-10-30 01:56:26 +0900

    `TtsSubscriptionDonation` now also stores a `TwitchSubscriberTier` instance

--- tts/ttsSubscriptionDonation.py
+++ tts/ttsSubscriptionDonation.py
@@ -2,10 +2,13 @@ try:
     import CynanBotCommon.utils as utils
     from CynanBotCommon.tts.ttsDonation import TtsDonation
     from CynanBotCommon.tts.ttsDonationType import TtsDonationType
+    from CynanBotCommon.twitch.twitchSubscriberTier import TwitchSubscriberTier
 except:
     import utils
     from tts.ttsDonation import TtsDonation
     from tts.ttsDonationType import TtsDonationType
+
+    from twitch.twitchSubscriberTier import TwitchSubscriberTier
 
 
 class TtsSubscriptionDonation(TtsDonation):
@@ -13,15 +16,22 @@ class TtsSubscriptionDonation(TtsDonation):
     def __init__(
         self,
         isAnonymous: bool,
-        isGift: bool
+        isGift: bool,
+        tier: TwitchSubscriberTier
     ):
         if not utils.isValidBool(isAnonymous):
             raise ValueError(f'isAnonymous argument is malformed: \"{isAnonymous}\"')
         elif not utils.isValidBool(isGift):
             raise ValueError(f'isGift argument is malformed: \"{isGift}\"')
+        elif not isinstance(tier, TwitchSubscriberTier):
+            raise ValueError(f'tier argument is malformed: \"{tier}\"')
 
         self.__isAnonymous: bool = isAnonymous
         self.__isGift: bool = isGift
+        self.__tier: TwitchSubscriberTier = tier
+
+    def getTier(self) -> TwitchSubscriberTier:
+        return self.__tier
 
     def getType(self) -> TtsDonationType:
         return TtsDonationType.SUBSCRIPTION
