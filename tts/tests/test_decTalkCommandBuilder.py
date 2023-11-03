3514209d9496a2da1a13bb32e8cd74ab49511d59
Charles Madere <charlesrmadere@gmail.com>
2023-11-03 16:40:14 +0900

    removed `say.exe` strings from tests

--- tts/tests/test_decTalkCommandBuilder.py
+++ tts/tests/test_decTalkCommandBuilder.py
@@ -76,62 +76,62 @@ class TestDecTalkCommandBuilder():
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDangerousCharactersString(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('& cd C:\\ & dir')
-        assert result == 'say.exe cd C:\\ dir'
+        assert result == 'cd C:\\ dir'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString1(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-post hello')
-        assert result == 'say.exe hello'
+        assert result == 'hello'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString2(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-pre hello')
-        assert result == 'say.exe hello'
+        assert result == 'hello'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString3(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-l hello')
-        assert result == 'say.exe hello'
+        assert result == 'hello'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString4(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-lw hello')
-        assert result == 'say.exe hello'
+        assert result == 'hello'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString5(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-l[t] hello')
-        assert result == 'say.exe hello'
+        assert result == 'hello'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString6(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-v show version information')
-        assert result == 'say.exe show version information'
+        assert result == 'show version information'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString7(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-d userDict')
-        assert result == 'say.exe userDict'
+        assert result == 'userDict'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDecTalkFlagsString8(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('-lang uk hello world')
-        assert result == 'say.exe hello world'
+        assert result == 'hello world'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withDirectoryTraversalString(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('& cd .. & dir')
-        assert result == 'say.exe cd dir'
+        assert result == 'cd dir'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withExtraneousSpacesString(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('  Hello,    World! ')
-        assert result == 'say.exe Hello, World!'
+        assert result == 'Hello, World!'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withHelloWorldString(self):
         result = await self.decTalkCommandBuilder.buildAndCleanCommand('Hello, World!')
-        assert result == 'say.exe Hello, World!'
+        assert result == 'Hello, World!'
 
     @pytest.mark.asyncio
     async def test_buildAndCleanCommand_withNone(self):
