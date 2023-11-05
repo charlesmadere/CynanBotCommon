f210d5d3c1c9c17a1a43a6adb13a98077abc34c4
Charles Madere <charlesrmadere@gmail.com>
2023-11-05 23:27:11 +0900

    be a bit more aggressive about removing "cheer" text from TTS

--- tts/decTalkCommandBuilder.py
+++ tts/decTalkCommandBuilder.py
@@ -59,7 +59,7 @@ class DecTalkCommandBuilder(TtsCommandBuilderInterface):
         self.__ttsSettingsRepository: TtsSettingsRepositoryInterface = ttsSettingsRepository
 
         self.__bannedStrings: List[Pattern] = self.__buildBannedStrings()
-        self.__cheerTextRegEx: Pattern = re.compile(r'^\s*cheer\d+\s*', re.IGNORECASE)
+        self.__cheerTextRegEx: Pattern = re.compile(r'(^|\s+)cheer\d+(\s+|$)', re.IGNORECASE)
         self.__whiteSpaceRegEx: Pattern = re.compile(r'\s{2,}', re.IGNORECASE)
 
     async def buildAndCleanEvent(self, event: Optional[TtsEvent]) -> Optional[str]:
@@ -93,7 +93,7 @@ class DecTalkCommandBuilder(TtsCommandBuilderInterface):
             self.__timber.log('DecTalkCommandBuilder', f'TTS command \"{message}\" returned a bad content code: \"{contentCode}\"')
             return None
 
-        message = self.__cheerTextRegEx.sub('', message)
+        message = self.__cheerTextRegEx.sub(' ', message)
 
         if not utils.isValidStr(message):
             return None
